f3dc2394162a29912e11f81a3887399662a5aa74
tlivigni <t.livigni@my-groom-service.com>
2024-02-25 12:34:26 +0100

    Add pseudo code resolution

---
+++ IP_Validation/resolution.js
@@ -0,0 +1,8 @@
+function isValidIP(str) {
+    //   si pas trois points false
+    //   si  ascii pas compris entre 48 et 57 false
+    //   si len = 0 false
+
+    //   faire un split sur . et verifier chaque sous tableau
+
+    }
