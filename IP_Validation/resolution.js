df90ddb701b24f4f0c32e0bb1cdce17d9ef7304c
tlivigni <t.livigni@my-groom-service.com>
2024-02-25 12:45:43 +0100

    update pseudo code resolution

--- IP_Validation/resolution.js
+++ IP_Validation/resolution.js
@@ -1,6 +1,6 @@
 function isValidIP(str) {
     //   si pas trois points false
-    //   si  ascii pas compris entre 48 et 57 false
+    //   si  ascii pas compris entre 48 et 57 false sauf .
     //   si len = 0 false
 
     //   faire un split sur . et verifier chaque sous tableau
