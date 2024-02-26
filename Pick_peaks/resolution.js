03c95abd866379c73a6a79b243da879b12640aaf
LordThi <thibaut.livigni@gmail.com>
2024-02-26 11:11:40 +0100

    Add pseudo code resolution

---
+++ Pick_peaks/resolution.js
@@ -0,0 +1,22 @@
+function pickPeaks(arr){
+    //pseudo-code
+    //parcourir le tableau pour trouver le premier pic :
+        // - comparer i avec i+1
+        // - stocker valeur+ dans une temp
+        // - stocker position pic
+        // - si val[i] === val[i++] alors stocker que val[i]
+        // - si val[0] > val[i] ne ps stocker
+        // - si val[arr.length] > val[i] ne pas stocker
+    // definir un creux pour trouver prochain pic:
+        // - comparer i avec i ++
+        // - stocker valeur plus basse
+        // - stocker position basse
+        // - si meme valeur entre pos[i] et pos[i++] stocker pos[i++]
+        // - si val[i] < val [j] alors nouveau creux à pos[j] -> trouver nouveau pic
+
+    arr.forEach(num => {
+        
+    });
+
+    //  return {pos:[],peaks:[]}
+}
